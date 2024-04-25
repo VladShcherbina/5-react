b4680ab7b5a5db9967e1d313aa4debdfb35fdd57
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-04-25 22:11:56 +0300

    5-react

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,16 +1,20 @@
-
+import { useNavigate } from 'react-router-dom';
 import styles from './ErrorPage.module.css'
 
 
 
 function ErrorPage() {
-   
+  const navigate = useNavigate()
 
+  const goBack = () => {
+    navigate('/')
+}
 
   return (
     <div className={styles.wrapper}>
-       <h1 className={styles.title}>404</h1>
+       <p className={styles.title}>404</p>
        <p className={styles.text}>Данна сторінка відсутня</p>
+       <button className={styles.goBackBtn} onClick={goBack}>Go Back</button>
     </div>
   );
   }
